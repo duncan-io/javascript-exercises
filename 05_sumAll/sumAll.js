e86be4248b696793724af1e4f104adb1961e1d0d
duncan <dmckee95@gmail.com>
2022-08-24 13:44:19 -0400

    Create function that adds numbers from an array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    const listOfNums=[];
 
+    // Check which value is greater and then create array of integers
+
+        if (typeof(num1)==="number" && typeof(num2)==="number" && num1 > 0 && num2 > 0 && num1 < num2){
+            for(let i = num1; i <= num2; i++){
+                listOfNums.push(i);
+            }
+        } else if (num2 < num1){
+            for(let i = num2; i <= num1; i++){
+                listOfNums.push(i);
+            }
+        } else {
+            return "ERROR"
+        }
+
+       
+  
+
+    // Add contents of newArray
+
+    let sum = 0;
+
+    for (let i=0; i < listOfNums.length; i++){
+        
+        sum += listOfNums[i];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
