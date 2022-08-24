329719f2da4d3be7906714a12e92abc2c87027a6
duncan <dmckee95@gmail.com>
2022-08-24 14:09:12 -0400

    Create conversion functions for f to c and c to f

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
 
+//convert farenheit to celsius
+const ftoc = function(f) {
+  let ce = ((f-32)*(5/9));
+  let ceRounded = Math.round(ce*10)/10;
+  return ceRounded;
 };
 
-const ctof = function() {
-
+//convert celsius to farenheit
+const ctof = function(c) {
+  let fe = c * (9/5) + 32;
+  let feRounded = Math.round(fe*10)/10;
+  return feRounded;
 };
 
 // Do not edit below this line
