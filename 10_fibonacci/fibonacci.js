719a7441ac5d3789a9cf6f9d9452bcc8f8091645
duncan <dmckee95@gmail.com>
2022-09-14 15:59:47 -0400

    Complete excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    y = parseInt(num);
 
+
+    if (y < 1) return "OOPS";
+
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+  
+
+    for(let i = 0; i < y; i++){
+        
+        nextTerm = n1 + n2;
+        n1 = n2; 
+        n2 = nextTerm; 
+        
+    }
+
+    return n1;
+    
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
