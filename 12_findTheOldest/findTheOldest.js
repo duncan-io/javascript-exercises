25e6644a7ab36133d5a36af7deb2020b0dcf4a37
duncan <dmckee95@gmail.com>
2022-09-15 15:34:42 -0400

    Complete excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(oldies) {
 
+    oldies.forEach(person => {
+        if(person.yearOfDeath == undefined){
+            person.yearOfDeath = (new Date()).getFullYear();
+        } 
+    })
+    
+    const oldest = oldies.sort((a, b) => {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+  
+        return lastGuy > nextGuy ? -1 : 1;
+       });
+
+
+       return oldest[0];
 };
 
 // Do not edit below this line
